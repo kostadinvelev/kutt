be8d55b55b3b16747d85a476d443246df27be095
Kostadin Velev <kostadin.velev@cleverpine.com>
2024-07-29 16:44:52 +0300

    fixed ssh key gen

--- main.tf
+++ main.tf
@@ -75,6 +75,7 @@ resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
 resource "aws_key_pair" "kutt_key_pair" {
   key_name   = var.ssh_key_name
   public_key = tls_private_key.example.public_key_openssh

0c51d3a5677704f311adf89b3277a21f218e6cf0
Kostadin Velev <kostadin.velev@cleverpine.com>
2024-07-30 09:40:57 +0300

    fixed setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
 
+LOGFILE="/tmp/setup.log"
+
+exec > >(tee -i $LOGFILE)
+exec 2>&1
+
 # Create Docker storage
-echo test > /tmp/test
+echo "Creating Docker storage..."
 sudo echo 'type=83' | sudo sfdisk /dev/xvdb
 sudo mkfs.xfs /dev/xvdb1
 sudo mkdir /app_docker_storage
 sudo mount /dev/xvdb1 /app_docker_storage
 
 # Install Docker and Git
+echo "Installing Docker and Git..."
 sudo yum update -y
 sudo yum install -y docker git
 
 # Configure Docker to use the new storage
-sudo sed -ie 's%DOCKER_STORAGE_OPTIONS=%DOCKER_STORAGE_OPTIONS="-g /app_docker_storage"%g' /etc/sysconfig/docker-storage
+echo "Configuring Docker storage..."
+sudo mkdir -p /etc/docker
+echo '{"data-root": "/app_docker_storage"}' | sudo tee /etc/docker/daemon.json
 
 # Enable and start Docker service
-sudo systemctl enable --now docker
-
-# Wait a bit to ensure Docker is fully started
-sleep 10
+echo "Enabling and starting Docker..."
+sudo systemctl enable docker
+sudo systemctl start docker
 
 # Add ec2-user to docker group
+echo "Adding ec2-user to docker group..."
 sudo usermod -aG docker ec2-user
 
 # Clone the Kutt repository
+echo "Cloning Kutt repository..."
 cd /app_docker_storage
 sudo -u ec2-user git clone https://github.com/thedevs-network/kutt
 cd kutt
 
 # Download the .docker.env and rename it to .env
-sudo -u ec2-user cp .docker.env .env
+echo "Downloading .docker.env..."
+sudo -u ec2-user wget https://raw.githubusercontent.com/thedevs-network/kutt/develop/.docker.env -O .env
 
 # Install Docker Compose
+echo "Installing Docker Compose..."
 wget https://github.com/docker/compose/releases/download/v2.23.1/docker-compose-linux-x86_64
 sudo install docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 
 # Ensure permissions are set correctly
+echo "Setting permissions..."
 sudo chown -R ec2-user:docker /app_docker_storage
 
 # Start the Kutt application
+echo "Starting Kutt application..."
 sudo -u ec2-user docker-compose up -d

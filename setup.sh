ff0703d33347bc385cb3e9292f3c485b73d9d04e
Kostadin Velev <kostadin.velev@cleverpine.com>
2024-07-30 10:16:07 +0300

    new script

--- setup.sh
+++ setup.sh
@@ -1,45 +1,27 @@
 #!/bin/bash
 
-LOGFILE="/tmp/setup.log"
+# Create Docker storage
+echo test > /tmp/test
+sudo echo 'type=83' | sudo sfdisk /dev/xvdh
+sudo mkfs.xfs /dev/xvdh1
+sudo mkdir /app_docker_storage
+sudo mount /dev/xvdb1 /app_docker_storage
 
-exec > >(tee -i $LOGFILE)
-exec 2>&1
-
-# Install Docker and Git
-echo "Installing Docker and Git..."
+# Install Docker
 sudo yum update -y
 sudo yum install -y docker git
 
-# Enable and start Docker service
-echo "Enabling and starting Docker..."
-sudo systemctl enable docker
-sudo systemctl start docker
+# Set Docker Storage to /k8s directory
+sudo sed -ie 's%OPTIONS=''%OPTIONS="-g /app_docker_storage"%g' /etc/sysconfig/docker
+
+# Enable and start Docker
+sudo systemctl enable --now docker
 
 # Add ec2-user to docker group
-echo "Adding ec2-user to docker group..."
 sudo usermod -aG docker ec2-user
-
-# Clone the Kutt repository
-echo "Cloning Kutt repository..."
-cd /home/ec2-user
-sudo -u ec2-user git clone https://github.com/thedevs-network/kutt
+git clone https://github.com/thedevs-network/kutt
 cd kutt
-
-# Download the .docker.env and rename it to .env
-echo "Downloading .docker.env..."
-sudo -u ec2-user wget https://raw.githubusercontent.com/thedevs-network/kutt/develop/.docker.env -O .env
-
-# Install Docker Compose
-echo "Installing Docker Compose..."
+cp .docker.env .env
 wget https://github.com/docker/compose/releases/download/v2.23.1/docker-compose-linux-x86_64
 sudo install docker-compose-linux-x86_64 /usr/local/bin/docker-compose
-
-# Start the Kutt application
-echo "Starting Kutt application..."
-sudo -u ec2-user docker-compose up -d
-
-# Check Docker status
-echo "Checking Docker status..."
-sudo systemctl status docker
-
-echo "Setup script completed."
+docker-compose up
